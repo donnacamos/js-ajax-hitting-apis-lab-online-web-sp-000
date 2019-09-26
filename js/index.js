aaa1da00a765131f210c26f7fc9a4cf39fa42c8c
Donna Amos <donnacamos.developer@gmail.com>
2019-09-26 03:33:02 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,4 +3,10 @@ const rootPath = 'https://api.github.com/'
 
 function getRepositories(page = 1){
   const username = document.querySelector('#username').value;
+  const req = new XMLHttpRequest();
+  let path = rootPath + `users/${username}/repos`
+  path = addQueries(path, {page: page, per_page: 100})
+  req.open('get', path);
+  req.addEventListener('load', () => displayRepositories.call(req, page));
+  req.send();
 }
